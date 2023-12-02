fb28d1213cea496c5fa0a2c19e5d582fdaa35acb
paulg44 <paulgarton1984@hotmail.com>
2023-12-02 16:42:34 +0000

    8kyu

---
+++ whose_move.js
@@ -0,0 +1,42 @@
+/* Two players - "black" and "white" are playing a game. The game consists of several rounds. If a player wins in a round, he is to move again during the next round. If a player loses a round, it's the other player who moves on the next round. Given whose turn it was on the previous round and whether he won, determine whose turn it is on the next round.
+
+Input/Output
+[input] string lastPlayer/$last_player
+
+"black" or "white" - whose move it was during the previous round.
+
+[input] boolean win/$win
+
+true if the player who made a move during the previous round won, false otherwise.
+
+[output] a string
+
+Return "white" if white is to move on the next round, and "black" otherwise.
+
+Example
+For lastPlayer = "black" and win = false, the output should be "white".
+
+For lastPlayer = "white" and win = true, the output should be "white". */
+
+// My solution
+function whoseMove(lastPlayer, win) {
+  //coding and coding..
+  if (lastPlayer === "black" && win === false) {
+    console.log("black lost");
+    return "white";
+  } else if (lastPlayer === "white" && win === false) {
+    console.log("white lost");
+    return "black";
+  } else if (lastPlayer === "black" && win === true) {
+    console.log("black won");
+    return "black";
+  } else if (lastPlayer === "white" && win === true) {
+    console.log("white won");
+    return "white";
+  }
+}
+
+// Best Practice/Clever
+function whoseMove(lastPlayer, win) {
+  return win ? lastPlayer : lastPlayer == "white" ? "black" : "white";
+}
